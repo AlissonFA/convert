87dc3c22e553dce0bc9d88a49bab33fca399a4cf
Alisson Freitas <alissonfreitas031@gmail.com>
2026-04-24 08:46:17 -0300

    Identificando a moeda

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Cotação de moedas do dia.
+const USD = 5.06
+const EUR = 5.93
+const GBP = 6.83
+
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form")
 const amount = document.querySelector("#amount")
@@ -14,5 +19,25 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
   event.preventDefault() // Desativa o comportamento padrão (impede reload).
 
-  console.log(currency.value)
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break;
+    default:
+      break;
+  }
+}
+
+// Função para converter a moeda.
+// amount: qual o valor total? (Ex: comprar 100 doláres)
+// price: qual o preço? (Ex: 1 dólar = R$ 5.06)
+// symbol: simbolo para exibir bonitinho.
+function convertCurrency(amount, price, symbol){
+  console.log(amount, price, symbol)
 }
